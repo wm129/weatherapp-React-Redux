5ef0593a599950896f5a3f71d8d7f9d0fd7211fe
wm129 <sekaimanyuucamp@gmail.com>
2020-12-30 23:51:13 +0900

    一例

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,2 +1,11 @@
-//Reducer 何もなし
+//Reducer 
+//Rank weathercityid 　２つのReducerへの参照をここにかく
+
+
+/*
+export { default as weathercityid } from './weathercityid';
+export { default as Rank } from './Rank'; 
+
+*/
+
 export const noop =( state = {}) => state;
