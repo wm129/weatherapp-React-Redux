2a4e98a597ac8d47adee787bae4bbc42c2c9c070
wm129 <sekaimanyuucamp@gmail.com>
2020-12-30 23:53:33 +0900

    一例

---
+++ src/reducers/weathercityid.js
@@ -0,0 +1,28 @@
+// JSONを常に返すReducer
+const initialState = {
+
+}
+
+
+
+
+/*
+const initialState = {
+    categories: [
+        {
+            id: '1',
+            name: 'all'
+        },
+        {
+            id: '2502',
+            name: 'other'
+        },
+        {
+            id: '10002',
+            name: 'other'
+        }
+    ]
+};
+export default () => initialState;
+
+*/
