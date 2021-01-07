1633a807a81f877ded62091839509fca5d277f37
wm129 <sekaimanyuucamp@gmail.com>
2021-01-07 18:24:37 +0900

    how to use axios

---
+++ src/actions/api_data.js
@@ -0,0 +1,37 @@
+//axios HTTP通信を扱う役割　JSONを取得、実装を簡易にする
+//JSで非同期処理を使う際　完了や失敗を表すオブジェクトにPromiseがある
+//pending: fulfilled: rejected : の3つのStateの１つをとる
+//特定のコードの実行完了をまってから次のコードを実行指示ができる
+
+import axios from "axios";
+
+export default fetchAPIResponse(city){
+    return function(dispatch){
+        axios.get("API").then(response=>{
+            let arr = [];
+            for (var key in response.data.location) {
+                arr.push(response.data.location[key]);
+            }
+            //dispatch actionの設定
+            dispatch({type:"FETCH_LOCATION", payload:arr});
+            let arr2 = [];
+            for (var key2 in response.data.current) {
+                arr2.push(response.data.current[key2]);
+            }
+            console.log(arr2);
+            arr2.splice(5, 1);
+            //dispatch actionの設定
+            dispatch({type:"FETCH_WEATHER", payload:arr2});
+            let arr3 = [];
+            for (var key3 in response.data.current.condition) {
+              arr3.push(response.data.current.condition[key3]);
+            }
+            //dispatch actionの設定
+            dispatch({type:"FETCH_CONDITIONS", payload:arr3});
+
+            console.log(response);
+        }).catch(err=>{
+            console.log(err)
+        });
+    };
+}
